d1e6b1bb3ac6bd19624dd1d44859f07999896985
felipefurukawa <felpissss@gmail.com>
2023-06-24 22:03:23 -0300

    front-end

--- sistema-reciclagem/src/App.js
+++ sistema-reciclagem/src/App.js
@@ -1,69 +1,79 @@
-
+import { Avatar, Layout, Menu } from 'antd';
+import { Html5Outlined, 
+         UserOutlined, 
+         QuestionCircleOutlined,
+         DeleteOutlined  } from '@ant-design/icons';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ComponentesAntd } from './ComponentesAntd';
+import { ComponentesAntd2 } from './ComponentesAntd2';
 import './App.css';
-import { Layout, Avatar, Menu } from 'antd'
-import { LoadingOutlined, SmileOutlined, QqOutlined  } from '@ant-design/icons';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
-
-
-const { Header, Footer, Sider, Content } = Layout;
-
-
 
 function App() {
 
-  const rotas = createBrowserRouter([
-    {
-        path: "/",
-        element: <h1>Ola</h1>
-    },
-      {
-        path: "/usuario",
-        element: <h1>Usuario</h1>
-    },
-
-
-    ]);
-  
+  const rotas = createBrowserRouter([{
+    path: "/",
+    element: <ComponentesAntd />
+  },
+  {
+    path: "/usuario",
+    element: <ComponentesAntd2 />
+  },
+  {
+    path: "/duvida",
+    element: <h1>Duvida</h1>
+  },
+  {
+    path: "/excluir",
+    element: <h1>Excluir</h1>
+  }]);
 
   const Conteudo = (props) => {
-      return  <RouterProvider router={rotas} />
-   }
-   const ItemMenuSelecionado = (item) => {
-      rotas.navigate(item.key);
+
+    return <RouterProvider router={rotas} />
+ 
    }
 
+  const itemMenuSelecionado = (item) => {
+    rotas.navigate(item.key);
+  }
 
   return (
+    <Layout style={{height: "100vh"}}>
+      <Layout.Header style={{color: "white"}}>
 
-  <Layout style={{height: "100vh"}}>
-    <Header>
-      <Avatar icon={ <LoadingOutlined /> }
-             style={{float: "right", marginTop: "15px"}}
-      />
-      <h1 style={{color: "gray", marginTop: "2px"}}>Header</h1>
-    </Header>
+      <div style={{display:"flex", height: 50,
+      flexDirection:"row", 
+      alignContent: "center", 
+      alignItems: "center"
+      }}>
+      <Avatar icon={ <Html5Outlined /> } />
+      <h1>Header</h1>
 
-  <Layout>
-    <Sider>
-      <Menu theme="dark" onClick={ItemMenuSelecionado}>
-        <Menu.Item key="/usuario" icon={<SmileOutlined />} >Usuários</Menu.Item>
-        <Menu.Item key="/pinguim" icon={<QqOutlined />} >Pinguins</Menu.Item>
-      </Menu>
-    </Sider>
+      </div>
+      </Layout.Header>
 
+      <Layout>
+        <Layout.Sider>
+          <Menu theme='dark' onClick={itemMenuSelecionado}>
+            <Menu.Item key="/usuario" icon={<UserOutlined />}>Usuarios</Menu.Item>
+            <Menu.Item key="/duvida" icon={<QuestionCircleOutlined />}>Duvidas</Menu.Item>
+            <Menu.Item key="/excluir" icon={<DeleteOutlined />}>Excluir</Menu.Item>
+          </Menu>
+        </Layout.Sider>
 
-    <Content>
-      <Conteudo/>
-    </Content>
+        <Layout.Content>
+          <div style={{marginLeft: 10}}>
+            <Conteudo />
+          </div>
+        </Layout.Content>
+        
+      </Layout>
 
+      <Layout.Footer><h1>Rodapé</h1></Layout.Footer>
     </Layout>
-    <Footer>
-      <h1>Rodape</h1>
-    </Footer>
-  </Layout>  
-);
+  );
 
-   
+
 
 }
 
